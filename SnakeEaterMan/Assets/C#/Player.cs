c6dc926036b12094dc47710508b57c180116f03d
erikusia <erikusia@icloud.com>
2021-02-27 21:52:42 +0900

    ちょっといじった

---
+++ SnakeEaterMan/Assets/C#/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody>().velocity.x = x * speed;
+        GetComponent<Rigidbody>().velocity.z = y * speed;
+
+        if (Input.GetKeyDown("joystick button 3"))
+        {
+            Debug.Log("button3");
+        }
+    }
+}
