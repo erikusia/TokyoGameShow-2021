eed7a0993441314fbf1d0f44db7ee83fa6b300a4
erikusia <erikusia@icloud.com>
2021-03-01 12:53:51 +0900

    ダッシュに制限を付けた

--- SnakeEaterMan/Assets/C#/Player.cs
+++ SnakeEaterMan/Assets/C#/Player.cs
@@ -8,9 +8,12 @@ public class Player : MonoBehaviour
     public float m_walk = 10;
     public float m_dash = 7;
     public float m_rotato = 2;
+    public float m_dashTimePlus = 0.1f;
     private float m_speed;
     Vector3 local_angle;
     bool dash=false;
+    float dashTime = 1;
+    float waitTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,17 +41,38 @@ public class Player : MonoBehaviour
 
         if (Input.GetKey("joystick button 3"))
         {
+            waitTime = 0;
             dash = true;
-            Debug.Log("ダッシュ");
         }
         else dash = false;
 
-        if (dash == true)
+        if (dash == true && dashTime > 0.1)
         {
             m_speed = m_dash;
+            dashTime -= Time.deltaTime;
+            Debug.Log("ダッシュ");
         }
-        else m_speed = m_walk;
+        else
+        {
+            
+            if (dashTime < 1) 
+            {
+                waitTime+= Time.deltaTime;
 
-        Debug.Log(m_speed);
+                if(waitTime > 1)
+                {
+                    dashTime += m_dashTimePlus;
+                    if (dashTime > 1)
+                    {
+                        dashTime = 1;
+                        waitTime = 0;
+                    }
+                }
+                
+
+            }
+            m_speed = m_walk;
+        }
+        Debug.Log(dashTime);
     }
 }
