3b705b1739aa2ae58d564c93b53eedc7f3bd074d
erikusia <erikusia@icloud.com>
2021-03-01 11:54:40 +0900

    移動の方法を改善

--- SnakeEaterMan/Assets/C#/Player.cs
+++ SnakeEaterMan/Assets/C#/Player.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
-    public float m_speed = 5;
+    public float m_walk = 10;
+    public float m_dash = 7;
+    public float m_rotato = 2;
+    private float m_speed;
     Vector3 local_angle;
+    bool dash=false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +28,27 @@ public class Player : MonoBehaviour
         if(x!=0||z!=0)
         {
             // 矢印キーが押されている方向にプレイヤーを移動する
-            var velocity = new Vector3(x, 0, z) / m_speed;
-            transform.localPosition += velocity;
+            transform.localPosition += transform.forward / m_speed;
 
+            //回転
             var direction = new Vector3(x, 0, z);
-            transform.localRotation = Quaternion.LookRotation(direction);
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.localRotation = Quaternion.Slerp(myTransform.localRotation,targetRotation,Time.deltaTime* m_rotato);
         }
+
+        if (Input.GetKey("joystick button 3"))
+        {
+            dash = true;
+            Debug.Log("ダッシュ");
+        }
+        else dash = false;
+
+        if (dash == true)
+        {
+            m_speed = m_dash;
+        }
+        else m_speed = m_walk;
+
+        Debug.Log(m_speed);
     }
 }
