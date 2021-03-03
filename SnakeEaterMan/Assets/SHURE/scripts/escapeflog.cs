59091c4036f5bfcf08cc744f03e16ce45e0d2d5e
KuroShure <rekuiemu9@gmail.com>
2021-03-03 21:16:57 +0900

    カエルがプレイヤーを追っかけてくるバージョンを追加(笑)

---
+++ SnakeEaterMan/Assets/SHURE/scripts/escapeflog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class escapeflog : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    NavMeshAgent agent;
+
+    //プレイヤーと逆の方向に向く
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
