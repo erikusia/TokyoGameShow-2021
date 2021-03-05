50ecc3369c8fb479174a4fa601afac219f364173
KuroShure <rekuiemu9@gmail.com>
2021-03-05 14:03:50 +0900

    カエル逃走する用

--- SnakeEaterMan/Assets/SHURE/scripts/escapeflog.cs
+++ SnakeEaterMan/Assets/SHURE/scripts/escapeflog.cs
@@ -6,14 +6,17 @@ using UnityEngine.AI;
 public class escapeflog : MonoBehaviour
 {
     [SerializeField]
-    public Transform target;//目標
+    public Transform target;//目標(プレイヤー
     public NavMeshAgent agent;//動くために必要な物
+    [SerializeField]
+    public Transform runpoint;//目標（帰宅するポイント
 
     public float distance;//距離
-
+    //GameObject movepos;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        //    runpoint = GetComponent<NavMeshAgent>();
     }
 
     void Update()
@@ -22,16 +25,19 @@ public class escapeflog : MonoBehaviour
         distance = Vector3.Distance(transform.position,
             target.transform.position);//距離を計算
         //Debug.Log(distance);
-        if(distance < 5)
+        if (distance < 14)
         {
             //5より近い場合追尾
             //ここのagent.destination -= target.transform.positionを
             //　-=　にしていることで、近くに来たら逃げるを実装
-            //細かいのはまだ
             agent.destination -= target.transform.position;
         }
+        else
+        {
+            agent.destination = runpoint.transform.position;
+        }
         //距離が１以上の時、プレイヤーの近くで停止する
-        if(distance <1)
+        if (distance < 2)
         {
             agent.destination = target.transform.position;
         }
