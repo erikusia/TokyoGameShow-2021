fb7949035051088c1ce1ff76c704630c20aadb56
佐藤幹 <->
2021-03-10 17:43:52 +0900

    エンドシーンからタイトルへ、ゲーム終了ボタン追加

---
+++ SnakeEaterMan/Assets/kan/Scripts/ReturnToScene.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReturnToScene : MonoBehaviour
+{
+    [SerializeField]
+    private Button titleButton;
+    [SerializeField]
+    private Button gameEndButton;
+
+    int buttonCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボタン選択
+        ButtonInput();
+        //
+        OnButton(buttonCount);
+    }
+
+    void ButtonInput()
+    {
+        float y = Input.GetAxis("Vertical1");
+        if (y > 0)
+            buttonCount = 0;
+        if (y < 0)
+            buttonCount = 1;
+    }
+
+    void OnButton(int count)
+    {
+        switch (count)
+        {
+            //タイトルボタンを表示
+            case 0:
+                titleButton.interactable = true;
+                gameEndButton.interactable = false;
+                //Bボタン押したら
+                if (Input.GetKey("joystick button 1"))
+                {
+                    titleButton.GetComponent<TitleButton>().OnClick();
+                }
+                break;
+            //エンディングボタンを表示
+            case 1:
+                titleButton.interactable = false;
+                gameEndButton.interactable = true;
+                //Bボタン押したら
+                if (Input.GetKey("joystick button 1"))
+                {
+                    gameEndButton.GetComponent<GameEndButton>().OnClick();
+                }
+                break;
+        }
+    }
+}
