4da150b69cbdb20f48eb8fbc1680c1363791cf1f
佐藤幹 <->
2021-04-16 05:53:09 +0900

    コントローラーの接続数に応じてAIを実装

--- SnakeEaterMan/Assets/kan/Scripts/PlayerStatus/HowToPlayer.cs
+++ SnakeEaterMan/Assets/kan/Scripts/PlayerStatus/HowToPlayer.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class HowToPlayer : MonoBehaviour
 {
     private int playerNum;
     private bool[] isPlayer = new bool[4];
+    private GameObject[] gameObjects = new GameObject[4];
 
     public GUIStyleState styleState;
     private GUIStyle style;
@@ -19,13 +21,16 @@ public class HowToPlayer : MonoBehaviour
         style = new GUIStyle();
         styleState.textColor = new Color(0, 0, 0, 1);
         isGUI = false;
+
+        gameObjects[0] = GameObject.Find("Player1");
+        gameObjects[1] = GameObject.Find("Player2");
+        gameObjects[2] = GameObject.Find("Player3");
+        gameObjects[3] = GameObject.Find("Player4");
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         for (int i = 0; i < isPlayer.Length; i++)
         {
             if (isPlayer[i])
@@ -35,6 +40,8 @@ public class HowToPlayer : MonoBehaviour
                 if (Input.GetButtonDown("A" + n.ToString()))
                 {
                     Time.timeScale = 1;
+                    //Aiを使う
+                    OnAI();
                     isPlayer[i] = false;
                     isGUI = true;
                 }
@@ -82,6 +89,23 @@ public class HowToPlayer : MonoBehaviour
             {
                 style.normal = styleState;
                 GUI.Label(new Rect(100 * i, 20, 100, 50), (i + 1).ToString() + "Player準備完了、Aボタンでスタート", style);
+            }
+        }
+    }
+
+    private void OnAI()
+    {
+        for (int j = 0; j < isPlayer.Length; j++)
+        {
+            if (!isPlayer[j])
+            {
+                gameObjects[j].GetComponent<PlayerMove>().playerNumber = 0;
+            }
+            else
+            {
+                gameObjects[j].GetComponent<hebi_ai>().enabled = false;
+                gameObjects[j].GetComponent<NavMeshAgent>().enabled = false;
+                gameObjects[j].transform.Find("group1").GetComponentInChildren<Snake_Ai>().enabled = false;
             }
         }
     }
