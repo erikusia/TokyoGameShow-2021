de8d4f51fd1b702213abac37f15f414b217f3ef7
KuroShure <rekuiemu9@gmail.com>
2021-05-13 17:33:31 +0900

    クリアシーンを最低限追加

--- SnakeEaterMan/Assets/SHURE/scripts/ClearScene.cs
+++ SnakeEaterMan/Assets/SHURE/scripts/ClearScene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -19,12 +20,17 @@ public class ClearScene : MonoBehaviour
     //カメラ格納場所
     private GameObject[] game_camera = new GameObject[4];
     float peke = 0.0f;//サイズ拡大用
-    float size_check = 0.0f;//サイズ調整用
+    float size_check;//サイズ調整用
 
     //文字表示
     [SerializeField]
     private GameObject clear_text;
+    [SerializeField]
+    private GameObject retry_exit;
 
+    //マップ消す
+    [SerializeField]
+    private GameObject maap;
     void Start()
     {
         //ここでオブジェクトににプレイヤーカメラを入れている。
@@ -48,8 +54,20 @@ public class ClearScene : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
 
     }
-    void Update()
+    void Quit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+    void Update()
+    {   
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            winner = "Player1";
+        }
         //プレイヤー分のマテリアルが白か透明ではなかった場合誰かがクリアしている.
         foreach (var mat in matName)
         {
@@ -57,84 +75,106 @@ public class ClearScene : MonoBehaviour
             if (!mat.transform.Find("group1").Find("Body1").GetComponent<SkinnedMeshRenderer>().material.name.Contains("White") &&
                 !mat.transform.Find("group1").Find("Body1").GetComponent<SkinnedMeshRenderer>().material.name.Contains("Transparency"))
             {
-                //cameras[0].gameObject.layer =  1 ; //cameras[1].gameObject.layer =  1;                //cameras[2].gameObject.layer =  1;//cameras[3].gameObject.layer =  1;
+                maap.SetActive(false);
                 winner = mat.name;
-
-                //Debug.Log(size_check);
-                //勝ったらカメラがワイドになる
-                if (winner == "Player1")
+                if (Input.GetKeyDown("joystick button 2"))
                 {
-                    size_check -= 0.001f;//ｘ、ｙ
-                    peke += 0.006f;//ｗ、ｈ
-                    // Debug.Log("1の勝利");
-                    //画面配置場所：左上
-                    cameras[0].rect = new Rect(0.0f, 0.5f - size_check,
-                        0.5f + peke, 0.5f + peke);
-                    //深度設定（画面優先順位変更　前面　１　～　ー１　背面
-                    cameras[0].depth = 1.0f;
-                    cameras[1].depth = -1.0f;
-                    cameras[2].depth = -1.0f;
-                    cameras[3].depth = -1.0f;
-                    Text clear = clear_text.GetComponent<Text>();
-                    clear.text = ("WINNER-" + winner + "-");
-
+                    SceneManager.LoadScene("GamePlay");
                 }
-                if (winner == "Player2")
+                if (Input.GetKeyDown("joystick button 3"))
                 {
-                    //画面配置場所：右上
-                    size_check -= 0.001f;//ｘ、ｙ
-                    peke += 0.006f;//ｗ、ｈ
-                    cameras[1].rect = new Rect(0.5f + size_check,
-                        0.5f + size_check, 0.5f + peke, 0.5f + peke);
-                    cameras[0].depth = -1.0f;
-                    cameras[1].depth = 1.0f;
-                    cameras[2].depth = -1.0f;
-                    cameras[3].depth = -1.0f;
-                    Text clear = clear_text.GetComponent<Text>();
-                    clear.text = ("WINNER-" + winner + "-");
-                    // Debug.Log("2の勝利");
-                }
-                if (winner == "Player3")
-                {
-                    size_check -= 0.001f;//ｘ、ｙ
-                    peke += 0.006f;//ｗ、ｈ
-                    //画面配置場所：左下
-                    cameras[2].rect = new Rect(0.0f, 0.0f,
-                        0.5f + peke, 0.5f + peke);
-                    cameras[0].depth = -1.0f;
-                    cameras[1].depth = -1.0f;
-                    cameras[2].depth = 1.0f;
-                    cameras[3].depth = -1.0f;
-                    //Debug.Log("3の勝利");
-                    Text clear = clear_text.GetComponent<Text>();
-                    clear.text = ("WINNER-" + winner + "-");
-                }
-                if (winner == "Player4")
-                {
-                    size_check -= 0.001f;//ｘ、ｙ
-                    peke += 0.006f;//ｗ、ｈ
-                    //画面配置場所：右下
-                    cameras[3].rect = new Rect(0.0f, 0.0f,
-                        0.5f + peke, 0.5f + peke);
-                    cameras[0].depth = -1.0f;
-                    cameras[1].depth = -1.0f;
-                    cameras[2].depth = -1.0f;
-                    cameras[3].depth = 1.0f;
-                    Text clear = clear_text.GetComponent<Text>();
-                    clear.text = ("WINNER-" + winner + "-");
-                    //Debug.Log("4の勝利");
-                }
-                if (peke > 1.0f)
-                {
-                    peke = 1.0f;
-                }
-                if (size_check < 0)
-                {
-                    size_check = 0;
+                    Quit();
                 }
                 //  Debug.Log("GameClear " + mat.name + " が勝ち");
                 audioSource.PlayOneShot(clips);
             }
+            if (winner == "Player1")
+            {
+                size_check += 0.005f;//ｘ、ｙ
+                peke += 0.006f;//ｗ、ｈ
+                               // Debug.Log("1の勝利");
+                               //画面配置場所：左上
+                cameras[0].rect = new Rect(0.0f, 0.5f - size_check, 0.5f + peke, 0.5f + peke);
+                if (cameras[0].rect.y < 0)
+                {
+                    cameras[0].rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+                }
+                //深度設定（画面優先順位変更　前面　１　～　ー１　背面
+                cameras[0].depth = 1.0f;
+                cameras[1].depth = -1.0f;
+                cameras[2].depth = -1.0f;
+                cameras[3].depth = -1.0f;
+                Text clear = clear_text.GetComponent<Text>();
+                clear.text = ("WINNER-" + winner + "-");
+                Text re = retry_exit.GetComponent<Text>();
+                re.text = ("Retry_X・Exit_Y");
+            }
+            if (winner == "Player2")
+            {
+                //画面配置場所：右上
+                size_check += 0.005f;//ｘ、ｙ
+                peke += 0.006f;//ｗ、ｈ
+                cameras[1].rect = new Rect(0.5f - size_check,
+                    0.5f - size_check, 0.5f + peke, 0.5f + peke);
+                if (cameras[1].rect.x < 0)
+                {
+                    cameras[1].rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+                }
+                cameras[0].depth = -1.0f;
+                cameras[1].depth = 1.0f;
+                cameras[2].depth = -1.0f;
+                cameras[3].depth = -1.0f;
+                Text clear = clear_text.GetComponent<Text>();
+                clear.text = ("WINNER-" + winner + "-");
+                Text re = retry_exit.GetComponent<Text>();
+                re.text = ("Retry_X・Exit_Y");
+                // Debug.Log("2の勝利");
+            }
+            if (winner == "Player3")
+            {
+                size_check -= 0.001f;//ｘ、ｙ
+                peke += 0.006f;//ｗ、ｈ
+                               //画面配置場所：左下
+                cameras[2].rect = new Rect(0.0f, 0.0f,
+                    0.5f + peke, 0.5f + peke);
+
+                cameras[0].depth = -1.0f;
+                cameras[1].depth = -1.0f;
+                cameras[2].depth = 1.0f;
+                cameras[3].depth = -1.0f;
+                //Debug.Log("3の勝利");
+                Text clear = clear_text.GetComponent<Text>();
+                clear.text = ("WINNER-" + winner + "-");
+                Text re = retry_exit.GetComponent<Text>();
+                re.text = ("Retry_X・Exit_Y");
+            }
+            if (winner == "Player4")
+            {
+                size_check -= 0.001f;//ｘ、ｙ
+                peke += 0.006f;//ｗ、ｈ
+                               //画面配置場所：右下
+                cameras[3].rect = new Rect(0.0f, 0.0f,
+                    0.5f + peke, 0.5f + peke);
+
+                cameras[0].depth = -1.0f;
+                cameras[1].depth = -1.0f;
+                cameras[2].depth = -1.0f;
+                cameras[3].depth = 1.0f;
+                Text clear = clear_text.GetComponent<Text>();
+                clear.text = ("WINNER-" + winner + "-");
+                Text re = retry_exit.GetComponent<Text>();
+                re.text = ("Retry_X・Exit_Y");
+                //Debug.Log("4の勝利");
+            }
+            if (peke > 1.0f)
+            {
+                peke = 1.0f;
+            }
+            if (size_check < 0)
+            {
+                size_check = 0;
+            }
+
         }
     }
 }
--- SnakeEaterMan/Assets/SHURE/scripts/TITLE/TITLE_SNAKE.cs
+++ SnakeEaterMan/Assets/SHURE/scripts/TITLE/TITLE_SNAKE.cs
@@ -4,42 +4,50 @@ using UnityEngine;
 
 public class TITLE_SNAKE : MonoBehaviour
 {
-    // Start is called before the first frame update
     private Transform SNAKEA;
-    private float Scene_Time;
+    private float Scene_Time;//time 
     bool flag = false;
-    //public  float speed = 0.0001f;
+
+    Animator anim;//アニメーション管理
 
     void Start()
     {
         SNAKEA = GetComponent<Transform>();
         Scene_Time = 0.0f;
-        //speed = 0.1f;
+        anim = this.gameObject.GetComponent<Animator>();
     }
-    // Update is called once per frame
     void Update()
     {
+
         if (Input.anyKeyDown)
         {
             flag = true;
         }
         if (flag == true)
         {
+            //anim.Play();
             Scene_Time += Time.deltaTime;
             Debug.Log(SNAKEA.transform.localPosition);
             if (Scene_Time > 0.5f)
             {
-                //Debug.Log("koko");
-                SNAKEA.transform.localPosition -= new Vector3(0.02f, 0.0f, 0.55f);
+                //Debug.Log("");
+                SNAKEA.transform.localPosition -= new Vector3(0.04f, 0.0f, 0.56f);
+                //anim.SetBool("MOVE", true);
+                //anim.SetBool("AGO", false);
             }
             if (Scene_Time > 3.5f)
             {
-                SNAKEA.transform.localPosition = new Vector3(0.0f, -3.0f, 117.0f);
+                SNAKEA.transform.localPosition = new Vector3(-1.0f, -37.0f, 160.0f);
+                //anim.SetBool("MOVE", false);
+                //anim.SetBool("AGO", true);
                 flag = false;
             }
         }
         if (flag == false)
         {
+            //anim.Stop();
+            //anim.SetBool("MOVE", false);
+            //anim.SetBool("AGO", false);
             Scene_Time = 0.0f;
         }
     }
