f1a6e7efaad1437767e731c4bef911bc9a60f301
KuroShure <rekuiemu9@gmail.com>
2021-05-13 18:03:53 +0900

    ゲームクリアシーンにクリアシーンを追加

--- SnakeEaterMan/Assets/SHURE/scripts/ClearScene.cs
+++ SnakeEaterMan/Assets/SHURE/scripts/ClearScene.cs
@@ -43,6 +43,11 @@ public class ClearScene : MonoBehaviour
         cameras[1] = game_camera[1].GetComponent<Camera>();
         cameras[2] = game_camera[2].GetComponent<Camera>();
         cameras[3] = game_camera[3].GetComponent<Camera>();
+        //初期座標
+        cameras[0].rect = new Rect(0.0f, 0.5f, 0.5f, 0.5f);
+        cameras[1].rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+        cameras[2].rect = new Rect(0.0f, 0.0f, 0.5f, 0.5f);
+        cameras[3].rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
 
         Application.targetFrameRate = 60;
         //プレイヤー分の名前をリストに入れる
@@ -52,7 +57,6 @@ public class ClearScene : MonoBehaviour
             matName.Insert(i - 1, GameObject.Find(pName));
         }
         audioSource = GetComponent<AudioSource>();
-
     }
     void Quit()
     {
@@ -63,11 +67,7 @@ public class ClearScene : MonoBehaviour
 #endif
     }
     void Update()
-    {   
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            winner = "Player1";
-        }
+    {
         //プレイヤー分のマテリアルが白か透明ではなかった場合誰かがクリアしている.
         foreach (var mat in matName)
         {
@@ -88,93 +88,92 @@ public class ClearScene : MonoBehaviour
                 //  Debug.Log("GameClear " + mat.name + " が勝ち");
                 audioSource.PlayOneShot(clips);
             }
-            if (winner == "Player1")
-            {
-                size_check += 0.005f;//ｘ、ｙ
-                peke += 0.006f;//ｗ、ｈ
-                               // Debug.Log("1の勝利");
-                               //画面配置場所：左上
-                cameras[0].rect = new Rect(0.0f, 0.5f - size_check, 0.5f + peke, 0.5f + peke);
-                if (cameras[0].rect.y < 0)
-                {
-                    cameras[0].rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
-                }
-                //深度設定（画面優先順位変更　前面　１　～　ー１　背面
-                cameras[0].depth = 1.0f;
-                cameras[1].depth = -1.0f;
-                cameras[2].depth = -1.0f;
-                cameras[3].depth = -1.0f;
-                Text clear = clear_text.GetComponent<Text>();
-                clear.text = ("WINNER-" + winner + "-");
-                Text re = retry_exit.GetComponent<Text>();
-                re.text = ("Retry_X・Exit_Y");
-            }
-            if (winner == "Player2")
-            {
-                //画面配置場所：右上
-                size_check += 0.005f;//ｘ、ｙ
-                peke += 0.006f;//ｗ、ｈ
-                cameras[1].rect = new Rect(0.5f - size_check,
-                    0.5f - size_check, 0.5f + peke, 0.5f + peke);
-                if (cameras[1].rect.x < 0)
-                {
-                    cameras[1].rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
-                }
-                cameras[0].depth = -1.0f;
-                cameras[1].depth = 1.0f;
-                cameras[2].depth = -1.0f;
-                cameras[3].depth = -1.0f;
-                Text clear = clear_text.GetComponent<Text>();
-                clear.text = ("WINNER-" + winner + "-");
-                Text re = retry_exit.GetComponent<Text>();
-                re.text = ("Retry_X・Exit_Y");
-                // Debug.Log("2の勝利");
-            }
-            if (winner == "Player3")
-            {
-                size_check -= 0.001f;//ｘ、ｙ
-                peke += 0.006f;//ｗ、ｈ
-                               //画面配置場所：左下
-                cameras[2].rect = new Rect(0.0f, 0.0f,
-                    0.5f + peke, 0.5f + peke);
-
-                cameras[0].depth = -1.0f;
-                cameras[1].depth = -1.0f;
-                cameras[2].depth = 1.0f;
-                cameras[3].depth = -1.0f;
-                //Debug.Log("3の勝利");
-                Text clear = clear_text.GetComponent<Text>();
-                clear.text = ("WINNER-" + winner + "-");
-                Text re = retry_exit.GetComponent<Text>();
-                re.text = ("Retry_X・Exit_Y");
-            }
-            if (winner == "Player4")
-            {
-                size_check -= 0.001f;//ｘ、ｙ
-                peke += 0.006f;//ｗ、ｈ
-                               //画面配置場所：右下
-                cameras[3].rect = new Rect(0.0f, 0.0f,
-                    0.5f + peke, 0.5f + peke);
-
-                cameras[0].depth = -1.0f;
-                cameras[1].depth = -1.0f;
-                cameras[2].depth = -1.0f;
-                cameras[3].depth = 1.0f;
-                Text clear = clear_text.GetComponent<Text>();
-                clear.text = ("WINNER-" + winner + "-");
-                Text re = retry_exit.GetComponent<Text>();
-                re.text = ("Retry_X・Exit_Y");
-                //Debug.Log("4の勝利");
-            }
-            if (peke > 1.0f)
-            {
-                peke = 1.0f;
-            }
-            if (size_check < 0)
-            {
-                size_check = 0;
-            }
-
         }
+        if (winner == "Player1")
+        {
+            size_check += 0.005f;//ｘ、ｙ
+            peke += 0.006f;//ｗ、ｈ
+                           //画面配置場所：左上
+            cameras[0].rect = new Rect(0.0f, 0.5f - size_check, 0.5f + peke, 0.5f + peke);
+            if (cameras[0].rect.y < 0)
+            {
+                cameras[0].rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+            }
+            //深度設定（画面優先順位変更　前面　１　～　ー１　背面
+            cameras[0].depth = 1.0f;
+            cameras[1].depth = -1.0f;
+            cameras[2].depth = -1.0f;
+            cameras[3].depth = -1.0f;
+            Text clear = clear_text.GetComponent<Text>();
+            clear.text = ("WINNER" + winner);
+            Text re = retry_exit.GetComponent<Text>();
+            re.text = ("Retry_X・Exit_Y");
+        }
+        if (winner == "Player2")
+        {
+            //画面配置場所：右上
+            size_check += 0.005f;//ｘ、ｙ
+            peke += 0.006f;//ｗ、ｈ
+            cameras[1].rect = new Rect(0.5f - size_check,
+                0.5f - size_check, 0.5f + peke, 0.5f + peke);
+            if (cameras[1].rect.x < 0)
+            {
+                cameras[1].rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+            }
+            cameras[0].depth = -1.0f;
+            cameras[1].depth = 1.0f;
+            cameras[2].depth = -1.0f;
+            cameras[3].depth = -1.0f;
+            Text clear = clear_text.GetComponent<Text>();
+            clear.text = ("WINNER" + winner);
+            Text re = retry_exit.GetComponent<Text>();
+            re.text = ("Retry_X・Exit_Y");
+            // Debug.Log("2の勝利");
+        }
+        if (winner == "Player3")
+        {
+            size_check -= 0.001f;//ｘ、ｙ
+            peke += 0.006f;//ｗ、ｈ
+                           //画面配置場所：左下
+            cameras[2].rect = new Rect(0.0f, 0.0f,
+                0.5f + peke, 0.5f + peke);
+
+            cameras[0].depth = -1.0f;
+            cameras[1].depth = -1.0f;
+            cameras[2].depth = 1.0f;
+            cameras[3].depth = -1.0f;
+            //Debug.Log("3の勝利");
+            Text clear = clear_text.GetComponent<Text>();
+            clear.text = ("WINNER" + winner);
+            Text re = retry_exit.GetComponent<Text>();
+            re.text = ("Retry_X・Exit_Y");
+        }
+        if (winner == "Player4")
+        {
+            size_check -= 0.001f;//ｘ、ｙ
+            peke += 0.006f;//ｗ、ｈ
+                           //画面配置場所：右下
+            cameras[3].rect = new Rect(0.0f, 0.0f,
+                0.5f + peke, 0.5f + peke);
+
+            cameras[0].depth = -1.0f;
+            cameras[1].depth = -1.0f;
+            cameras[2].depth = -1.0f;
+            cameras[3].depth = 1.0f;
+            Text clear = clear_text.GetComponent<Text>();
+            clear.text = ("WINNER" + winner);
+            Text re = retry_exit.GetComponent<Text>();
+            re.text = ("Retry_X・Exit_Y");
+            //Debug.Log("4の勝利");
+        }
+        if (peke > 1.0f)
+        {
+            peke = 1.0f;
+        }
+        if (size_check < 0)
+        {
+            size_check = 0;
+        }
+
     }
 }
