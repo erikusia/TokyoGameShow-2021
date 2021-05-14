c5c15bd252411a4c4db65f7cc5524d4540ae3532
KuroShure <rekuiemu9@gmail.com>
2021-05-14 10:15:30 +0900

    タイトルのヘビ調整

--- SnakeEaterMan/Assets/SHURE/scripts/TITLE/TITLE_SNAKE.cs
+++ SnakeEaterMan/Assets/SHURE/scripts/TITLE/TITLE_SNAKE.cs
@@ -7,6 +7,7 @@ public class TITLE_SNAKE : MonoBehaviour
     private Transform SNAKEA;
     private float Scene_Time;//time 
     bool flag = false;
+    private float speed=200.0f;
 
     Animator anim;//アニメーション管理
 
@@ -18,36 +19,30 @@ public class TITLE_SNAKE : MonoBehaviour
     }
     void Update()
     {
-
         if (Input.anyKeyDown)
         {
             flag = true;
         }
         if (flag == true)
         {
-            //anim.Play();
             Scene_Time += Time.deltaTime;
+            
             Debug.Log(SNAKEA.transform.localPosition);
             if (Scene_Time > 0.5f)
             {
-                //Debug.Log("");
-                SNAKEA.transform.localPosition -= new Vector3(0.04f, 0.0f, 0.56f);
-                //anim.SetBool("MOVE", true);
-                //anim.SetBool("AGO", false);
+
+                SNAKEA.transform.localPosition -= new Vector3(0.04f, 0.0f, 0.66f)*speed*Time.deltaTime;
             }
             if (Scene_Time > 3.5f)
             {
                 SNAKEA.transform.localPosition = new Vector3(-1.0f, -37.0f, 160.0f);
-                //anim.SetBool("MOVE", false);
-                //anim.SetBool("AGO", true);
+
                 flag = false;
             }
         }
         if (flag == false)
         {
-            //anim.Stop();
-            //anim.SetBool("MOVE", false);
-            //anim.SetBool("AGO", false);
+
             Scene_Time = 0.0f;
         }
     }
